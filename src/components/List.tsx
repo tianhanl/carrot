b356f513df8067acd6912ac645898ffa7ddaba9d
Tianhang Liu <tianhanl@usc.edu>
2018-10-30 12:04:00 -0700

    add style

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -14,6 +14,23 @@ interface ListComponentProps {
   items: [Item];
 }
 
+const grid: number = 8;
+
+const getItemStyle = (draggableStyle: any, isDragging: boolean): {} => ({
+  userSelect: 'none',
+  padding: 2 * grid,
+  margin: `0 0 ${grid}px 0`,
+  background: isDragging ? 'lightgreen' : 'grey',
+  ...draggableStyle
+});
+
+const getListStyle = (isDraggingOver: boolean): {} => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  padding: grid,
+  width: 300,
+  minHeight: 400
+});
+
 class List extends React.Component<ListComponentProps, {}> {
   public render() {
     const { droppableId, items } = this.props;
@@ -23,7 +40,7 @@ class List extends React.Component<ListComponentProps, {}> {
           <div
             ref={provided.innerRef}
             {...provided.droppableProps}
-            style={{ border: 'black 1px solid', height: 300, width: '20%' }}
+            style={getListStyle(snapshot.isDraggingOver)}
           >
             {items.map((item, index) => (
               <Draggable key={item.id} draggableId={item.id} index={index}>
@@ -36,6 +53,10 @@ class List extends React.Component<ListComponentProps, {}> {
                       ref={providedDraggable.innerRef}
                       {...providedDraggable.draggableProps}
                       {...providedDraggable.dragHandleProps}
+                      style={getItemStyle(
+                        providedDraggable.draggableProps.style,
+                        snapshotDraggable.isDragging
+                      )}
                     >
                       {item.content}
                     </div>
